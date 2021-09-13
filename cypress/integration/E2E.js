a3abd67e728733c4b5b4b29bc60783cdbb0aec62
Aleksi Lukkarinen <aleksi.lukkarinen@gmail.com>
2021-09-13 14:57:52 +0300

    Ex. 11.9

--- cypress/integration/E2E.js
+++ cypress/integration/E2E.js
@@ -5,6 +5,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('detail page of a pokemon can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.get(".list-item").contains("ivysaur").click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
+
 })
 
 
